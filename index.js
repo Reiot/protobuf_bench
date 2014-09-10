a53629e9035b76eb057aeecc870d792de7178572
Ray Yun <reiot94@gmail.com>
2014-09-10 14:49:21 +0900

    more test with sint32/sint64

--- index.js
+++ index.js
@@ -12,19 +12,53 @@ var LoginResponse = schema['packet.LoginResponse'];
 // console.log('Account', Account);
 // console.log('LoginResponse', LoginResponse);
 
-// protobuf.js
-var ProtoBuf = require('protobufjs');
-var builder = ProtoBuf.loadProtoFile(path.normalize('packet.proto'));
-var pb = builder.build('packet');
-
 // node-protobuf
 var node_protobuf = require("node-protobuf");
-var npb = new node_protobuf(fs.readFileSync("packet.desc"));
+var pb2 = new node_protobuf(fs.readFileSync("packet.desc"));
 
 // protocol-buffers
 var protocol_buffers = require('protocol-buffers');
 var pb3 = protocol_buffers(fs.readFileSync('packet.proto'));
 // console.log(pb3);
+
+// protobuf.js
+var ProtoBuf = require('protobufjs');
+var builder = ProtoBuf.loadProtoFile(path.normalize('packet.proto'));
+var pb4 = builder.build('packet');
+
+function diff(x, y) {
+    assert.equal(x.id, y.id);
+    if (y.accessToken)
+        assert.equal(x.accessToken, y.accessToken);
+
+    // error
+    // assert.equal(x.error.category, y.error.category);
+    if (y.error != null) {
+        if (y.error.message != null)
+            assert.equal(x.error.message, y.error.message);
+        if (y.error.code)
+            assert.equal(x.error.code, y.error.code);
+    }
+
+    // account
+    if (y.account != null) {
+        assert.equal(x.account.id, y.account.id);
+        assert.equal(x.account.userID, y.account.userID);
+    }
+
+    // items
+    if (y.items.length > 0) {
+        assert.equal(x.items.length, y.items.length);
+        for(var i = 0 ; i < x.items.length ; i ++) {
+            var a = x.items[i];
+            var b = y.items[i];
+
+            assert.equal(a.id, b.id);
+            assert.equal(a.category, b.category);
+            assert.equal(a.count, b.count);
+        }
+    }
+}
 
 function run(itemCount) {
 
@@ -41,6 +75,7 @@ function run(itemCount) {
 
     var json = {
         id: 123456789,
+        accessToken: "12345678901234567890",
         error: {
             category: 500,
             message: "invalid protocol buffer",
@@ -51,18 +86,45 @@ function run(itemCount) {
             userID: 'who@gmail.com',
             // what: 'the hell'
         },
-        accessToken: "12345678901234567890",
         items: items,
     };
 
-    var pbuf = new pb.LoginResponse(json);
 
     console.log('\n##', itemCount, 'items ##\n');
-    console.log('JSON:', JSON.stringify(json).length, 'bytes');
-    console.log('ProtoBuf.js:', pbuf.encodeNB().length, 'bytes');
-    console.log('node-protobuf:', npb.serialize(json, "packet.LoginResponse").length, 'bytes');
-    console.log('protobuf:', LoginResponse.serialize(json).length, 'bytes');
-    console.log('protocol-buffers:', pb3.LoginResponse.encode(json).length, 'bytes');
+
+    var encoded = {};
+    var decoded = {};
+
+    encoded['json'] = JSON.stringify(json);
+    decoded['json'] = JSON.parse(encoded['json']);
+
+    encoded['protobuf'] = LoginResponse.serialize(json);
+    decoded['protobuf'] = LoginResponse.parse(encoded['protobuf']);
+
+    encoded['node-protobuf'] = pb2.serialize(json, "packet.LoginResponse");
+    decoded['node-protobuf'] = pb2.parse(encoded['node-protobuf'], "packet.LoginResponse");
+
+    encoded['protocol-buffers'] = pb3.LoginResponse.encode(json);
+    decoded['protocol-buffers'] = pb3.LoginResponse.decode(encoded['protocol-buffers']);
+
+    encoded['protobufjs'] = (new pb4.LoginResponse(json)).encodeNB();
+    decoded['protobufjs'] = pb4.LoginResponse.decode(encoded['protobufjs']);
+
+    for(var k in encoded) {
+        console.log(k, ':', encoded[k].length, 'bytes');
+    }
+
+    for(var k2 in decoded) {
+        diff(decoded[k2], json);
+    }
+
+    // protobufjs/protocol-buffers encode sint32/sint64 with zigzag encoding (protobuf-net too)
+    diff(pb4.LoginResponse.decode(encoded['protocol-buffers']), json);
+    diff(pb3.LoginResponse.decode(encoded['protobufjs']), json);
+
+    // protobuf/node-protobuf does not.
+    diff(LoginResponse.parse(encoded['node-protobuf']), json);
+    diff(pb2.parse(encoded['protobuf'], "packet.LoginResponse"), json);
 
     //
     var suite = new Benchmark.Suite();
@@ -76,10 +138,10 @@ function run(itemCount) {
         LoginResponse.parse(LoginResponse.serialize(json));
     })
     .add('node-protobuf', function() {
-        npb.parse(npb.serialize(json, "packet.LoginResponse"), "packet.LoginResponse");
+        pb2.parse(pb2.serialize(json, "packet.LoginResponse"), "packet.LoginResponse");
     })
-    .add('ProtoBuf.js', function() {
-        pb.LoginResponse.decode(pbuf.encodeNB());
+    .add('protobufjs', function() {
+        pb4.LoginResponse.decode((new pb4.LoginResponse(json)).encodeNB());
     })
     .on('complete', function() {
         this.forEach(function(bench) {
